b1388333b22f5314be7fdb1a6aa9bd8a26cc2bc0
PHAMNIIT147 <phongpham663@gmail.com>
2020-04-30 17:58:49 +0700

    :tada: first commit app react Zik

---
+++ precache-manifest.2c44cabe8c220ac074ef9b2b42dcdb47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d55d3c916bfa070d75f503929164e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abd38295284e512798e3",
+    "url": "/static/css/main.2565c601.chunk.css"
+  },
+  {
+    "revision": "957eaffc902ac9dde7d7",
+    "url": "/static/js/2.379ef8b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.379ef8b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd38295284e512798e3",
+    "url": "/static/js/main.7fa64c13.chunk.js"
+  },
+  {
+    "revision": "0f2d7f21c04091c570b8",
+    "url": "/static/js/runtime-main.5d5ed2d1.js"
+  },
+  {
+    "revision": "2f2420d370294e30c885de915df6ad5a",
+    "url": "/static/media/logo.2f2420d3.svg"
+  }
+]);
